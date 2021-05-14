a3c412b9500ed13cf02309522459ac6e7b994b35
kial917 <kial917@mail.ru>
2021-05-14 11:52:30 +0300

    14.2 code has been clean up

--- js/app.js
+++ js/app.js
@@ -1,41 +1,33 @@
-for (var keys = document.querySelectorAll("#calculator span"), 
-operators = ["+", "-", "x", "÷"],
-clear = "C", // задекларировал переменную clear со значение C
-decimalAdded = !1, i = 0; i < keys.length; i++) 
+for (var keys = document.querySelectorAll("#calculator span"),
+        operators = ["+", "-", "x", "÷"],
+        clear = "C", // задекларировал переменную clear со значение C
+        decimalAdded = !1, i = 0; i < keys.length; i++)
 
-keys[i].onclick = function (e) {
-    var input = document.querySelector(".screen"),
-        inputVal = input.innerHTML,
-        btnVal = this.innerHTML,
-        total;
-
-    if ("=" == btnVal) {
-        var equation = inputVal,
-            lastChar = equation[equation.length - 1];
-        equation = equation.replace(/x/g, "*").replace(/÷/g, "/"), (operators.indexOf(lastChar) > -1 || "." == lastChar) && (equation = equation.replace(/.$/, "")), equation && (total = eval(equation), -1 != total.toString().indexOf(".") && (total = total.toFixed(2)), input.innerHTML = total), decimalAdded = !1
-    }   else if (operators.indexOf(btnVal) > -1) {
-        var lastChar = inputVal[inputVal.length - 1];
-        ("" != inputVal && -1 == operators.indexOf(lastChar) || "" == inputVal && "-" == btnVal) && (input.innerHTML += btnVal), operators.indexOf(lastChar) > -1 && inputVal.length > 1 && (input.innerHTML = inputVal.replace(/.$/, btnVal)), decimalAdded = !1
-    } else "." == btnVal ? decimalAdded || (input.innerHTML += btnVal, decimalAdded = !0) : input.innerHTML += btnVal;
-    
-    if (clear == btnVal){
-        console.log("hi");
-        
-                // input.innerHTML =0;
-                
-                input.innerHTML ="Жми сильнее";
-                
+    keys[i].onclick = function (e) {
+        var input = document.querySelector(".screen"),
+            inputVal = input.innerHTML,
+            btnVal = this.innerHTML,
+            total;
+            function outPutVal() {
                 function joke() {
-                    input.innerHTML ="Ладно,сброшусь)";
-                        function joke1() {
-                            input.innerHTML =""
-                        }
-                        setTimeout(joke1, 1000);
-                }
-                               
+                    input.innerHTML = "Ладно,сброшусь)";
+                    function joke1() {input.innerHTML = ""};
+                    setTimeout(joke1, 1000);
+                };
                 setTimeout(joke, 1000);
-                
-    } // Ввел условие срабатывания  и также следствия срабатывания (по аналогии с предущими условиями)
-    
-    e.preventDefault()
-};
+                };
+        if ("=" == btnVal) {
+            var equation = inputVal,
+                lastChar = equation[equation.length - 1];
+            equation = equation.replace(/x/g, "*").replace(/÷/g, "/"), (operators.indexOf(lastChar) > -1 || "." == lastChar) && (equation = equation.replace(/.$/, "")), equation && (total = eval(equation), -1 != total.toString().indexOf(".") && (total = total.toFixed(2)), input.innerHTML = total), decimalAdded = !1
+        } else if (operators.indexOf(btnVal) > -1) {
+            var lastChar = inputVal[inputVal.length - 1];
+            ("" != inputVal && -1 == operators.indexOf(lastChar) || "" == inputVal && "-" == btnVal) && (input.innerHTML += btnVal), operators.indexOf(lastChar) > -1 && inputVal.length > 1 && (input.innerHTML = inputVal.replace(/.$/, btnVal)), decimalAdded = !1
+        } else "." == btnVal ? decimalAdded || (input.innerHTML += btnVal, decimalAdded = !0) : input.innerHTML += btnVal;
+
+        if (clear == btnVal) {
+            input.innerHTML = "Жми сильнее";
+            outPutVal();
+        } // Ввел условие срабатывания  и также следствия срабатывания (по аналогии с предущими условиями)
+        e.preventDefault()
+    };
